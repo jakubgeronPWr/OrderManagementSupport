207b99e85aa4fbe409328c716a21e2d28e28b161
Jakub Geroń <190729@student.pwr.edu.pl>
2020-05-06 16:47:05 +0200

    services correction

--- OrderManagementSupport/ClientApp/src/app/shared/services/clients-rest.service.ts
+++ OrderManagementSupport/ClientApp/src/app/shared/services/clients-rest.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Client } from '../model/client';
+import { Order } from "../model/order";
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +12,17 @@ export class ClientsRestService {
 
   constructor(private readonly http: HttpClient) {
 
+  }
+
+  findAll(): Observable<Client[]> {
+    return this.http.get<Client[]>('/api/clients');
+  }
+
+  findBookById(id: number): Observable<Client> {
+    return this.http.get<Client>(`/api/clients/${id}`);
+  }
+
+  delete(clientId: number): Observable<Client> {
+    return this.http.delete<Client>(`/api/clients/${clientId}`);
   }
 }
--- OrderManagementSupport/ClientApp/src/app/shared/services/orders-rest.service.ts
+++ OrderManagementSupport/ClientApp/src/app/shared/services/orders-rest.service.ts
@@ -1,10 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Order } from '../model/order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersRestService {
 
-  constructor(private readonly http: HttpClient) { }
+  constructor(private readonly http: HttpClient) {
+
+  }
+
+  findAll(): Observable<Order[]> {
+    return this.http.get<Order[]>('/api/orders');
+  }
+
+  findBookById(id: string): Observable<Order> {
+    return this.http.get<Order>(`/api/orders/${id}`);
+  }
+
+  delete(bookId: string): Observable<Order> {
+    return this.http.delete<Order>(`/api/orders/${bookId}`);
+  }
+
 }
