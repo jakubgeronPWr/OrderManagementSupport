05674ab6ed913adc322a160a5452147a06bef91a
justyna_zieniewicz <zieniewicz.justyna@gmail.com>
2020-05-08 00:23:23 +0200

    add nav bar component

--- OrderManagementSupport/ClientApp/src/app/app.module.ts
+++ OrderManagementSupport/ClientApp/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { OrdersListComponent } from './orders/orders-list/orders-list.component';
 import { OrderAddComponent } from './orders/order-add/order-add.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OrdersListComponent,
-    OrderAddComponent
+    OrderAddComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
