66f7451c659f90594a24a3b63fa531a880993e62
Jakub Geroń <190729@student.pwr.edu.pl>
2020-05-09 19:42:29 +0200

    add sonnar

--- OrderManagementSupport/ClientApp/src/app/shared/services/clients-rest.service.ts
+++ OrderManagementSupport/ClientApp/src/app/shared/services/clients-rest.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Client } from '../model/client';
-import { Order } from "../model/order";
+
 
 @Injectable({
   providedIn: 'root'
