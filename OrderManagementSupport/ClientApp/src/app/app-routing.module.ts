b64edffbc590c22fd3a548dd4d8172d66f15f067
justyna_zieniewicz <zieniewicz.justyna@gmail.com>
2020-05-17 23:23:23 +0200

    wstępne ostylowanie nav-baru

--- OrderManagementSupport/ClientApp/src/app/app-routing.module.ts
+++ OrderManagementSupport/ClientApp/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { OrdersListComponent } from './orders/orders-list/orders-list.component';
+import { OrderAddComponent } from './orders/order-add/order-add.component';
 import { OrdersListResolver } from './orders/orders-list/orders-list.resolver';
 
 const routes: Routes = [
@@ -11,6 +12,10 @@ const routes: Routes = [
     resolve: {
       orders: OrdersListResolver
     }
+  },
+  {
+    path: 'order',
+    component: OrderAddComponent
   }
   ];
 
